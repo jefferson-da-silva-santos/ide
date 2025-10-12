d5ea7cb4fa54ce0095cdabab10b11e1e6853692e
jefferson-da-silva-santos <jeffrrwpg678@gmail.com>
2025-10-12 17:51:07 -0300

    feat: add new page ide

--- src/pages/IDE/index.jsx
+++ src/pages/IDE/index.jsx
@@ -1,7 +1,33 @@
+import React from "react";
+import { ReactTyped } from "react-typed";
+
 export const IDE = () => {
   return (
-    <div className="container-ide">
-      <h1>Teste</h1>
+    <div className="ide-container">
+      <div className="ide-overlay-father">
+        <div className="ide-overlay">
+          <h1 className="ide-title">
+            <ReactTyped
+              strings={[
+                '"Jesus não se esqueceu de você!! Ele ainda te espera, e te ama."',
+              ]}
+              typeSpeed={50}   // velocidade de digitação
+              backSpeed={30}   // velocidade de apagar
+              showCursor       // mostra o cursor piscando
+              cursorChar="|"   // caractere do cursor
+              loop={false}     // digita só uma vez
+            />
+          </h1>
+
+          <p className="ide-verse">
+            "Porque o filho do homem veio buscar e salvar o que se havia perdido"
+            <br />
+            <span className="ide-reference">Lc 19:26</span>
+          </p>
+
+          <p className="ide-speaker">Pr. Cândido de Freitas</p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
