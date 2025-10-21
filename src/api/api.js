8472e72388b9e48b362bfd251fff53d2e780751e
Jefferson Santos <jeffrrwpg678@gmail.com>
2025-10-21 16:01:30 -0300

    feat: adiciona o novo endpoint de prod

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://ide-api-276n.onrender.com/api/ide/v1",
+  baseURL: "https://ide-api.vercel.app/api/ide/v1",
   headers: {
     "Content-Type": "application/json"
   }
