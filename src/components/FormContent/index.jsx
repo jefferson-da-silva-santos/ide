25c8e64421d8fd1c7a93d8cbb10bd631f3402a17
jefferson-da-silva-santos <jeffrrwpg678@gmail.com>
2025-10-25 00:08:02 -0300

    feat: add

--- src/components/FormContent/index.jsx
+++ src/components/FormContent/index.jsx
@@ -70,209 +70,192 @@ export const FormContent = () => {
   return (
     <form onSubmit={formik.handleSubmit}  className="form-ide">
       <div className="header-form">
-        <h2>Formulário Evangelístico ✝️</h2>
+        <h2>Proclame a mensagem aqui! ✝️</h2>
       </div>
 
-      <div>
-        <label className="label-ide" style={{ display: "flex", alignItems: "center", gap: "8px" }}>
-          <InputSwitch
-            checked={showCurrentData}
-            onChange={toggleShowCurrentData}
+      <div className="form-content">
+        <div className="form-group">
+          <label className="label-ide" style={{ display: "flex", alignItems: "center", gap: "8px" }}>
+            <InputSwitch
+              checked={showCurrentData}
+              onChange={toggleShowCurrentData}
+            />
+            {showCurrentData
+              ? "Ocultar Dados Atuais"
+              : "Visualizar Dados Atuais 🔎"}
+          </label>
+        </div>
+        {showCurrentData && (
+          <p
+            style={{
+              width: "100%",
+              marginBottom: "15px",
+              textAlign: "center",
+              padding: "10px",
+              border: "1px solid #ccc",
+              borderRadius: "4px",
+            }}
+          >
+            Dados Atuais (Preview): Abaixo de cada campo você vê
+            o dado que está atualmente ativo.
+          </p>
+        )}
+        <div className="form-group">
+          <label className="label-ide" htmlFor="palavraEsperanca">Palavra de Esperança *</label>
+          <input
+            id="palavraEsperanca"
+            name="palavraEsperanca"
+            type="text"
+            placeholder='Ex: "Jesus te ama!"'
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.palavraEsperanca}
           />
-          {showCurrentData
-            ? "Ocultar Dados Atuais"
-            : "Visualizar Dados Atuais 🔎"}
-        </label>
-      </div>
-
-      {showCurrentData && (
-        <p
-          style={{
-            width: "100%",
-            marginBottom: "15px",
-            textAlign: "center",
-            padding: "10px",
-            border: "1px solid #ccc",
-            borderRadius: "4px",
-          }}
-        >
-          Dados Atuais (Preview): Abaixo de cada campo você vê
-          o dado que está atualmente ativo.
-        </p>
-      )}
-
-      <div className="form-group">
-        <label className="label-ide" htmlFor="palavraEsperanca">Palavra de Esperança *</label>
-
-        <input
-          id="palavraEsperanca"
-          name="palavraEsperanca"
-          type="text"
-          placeholder='Ex: "Jesus te ama!"'
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.palavraEsperanca}
-        />
-        {formik.touched.palavraEsperanca && formik.errors.palavraEsperanca && (
-          <span className="error">{formik.errors.palavraEsperanca}</span>
-        )}
-
-        <CurrentDataPreview
-          label={"mensagem atual:"}
-          value={apiData?.valor?.mensagem || ""}
-          loadingContent={loadingContent}
-          showCurrentData={showCurrentData}
-        />
-      </div>
-
-      <div className="form-group">
-        <label className="label-ide" htmlFor="textoBiblico">Texto Bíblico *</label>
-
-        <textarea
-          id="textoBiblico"
-          name="textoBiblico"
-          rows="4"
-          placeholder="Digite aqui o versículo ou texto bíblico"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.textoBiblico}
-        ></textarea>
-        {formik.touched.textoBiblico && formik.errors.textoBiblico && (
-          <span className="error">{formik.errors.textoBiblico}</span>
-        )}
-
-        <CurrentDataPreview
-          label={"versiculo atual:"}
-          value={apiData?.valor?.versiculo || ""}
-          loadingContent={loadingContent}
-          showCurrentData={showCurrentData}
-          isTextArea={true}
-        />
-      </div>
-
-      <div className="form-group">
-        <label className="label-ide" htmlFor="referencia">Referência Bíblica *</label>
-
-        <input
-          id="referencia"
-          name="referencia"
-          type="text"
-          placeholder='Ex: "João 3:16"'
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.referencia}
-        />
-        {formik.touched.referencia && formik.errors.referencia && (
-          <span className="error">{formik.errors.referencia}</span>
-        )}
-        <CurrentDataPreview
-          label={"referência atual:"}
-          value={apiData?.valor?.referencia || ""}
-          loadingContent={loadingContent}
-          showCurrentData={showCurrentData}
-        />
-      </div>
-
-      <div className="form-group">
-        <label className="label-ide" htmlFor="background">Imagem de Fundo *</label>
-
-        <select
-          id="background"
-          name="background"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.background}
-          className={
-            formik.touched.background && formik.errors.background
-              ? "input-error"
-              : ""
-          }
-        >
-          <option value="" disabled>
-            Selecione uma opção
-          </option>
-          {backgroundOptions.map((option) => (
-            <option key={option.value} value={option.value}>
-              {option.label}
+          {formik.touched.palavraEsperanca && formik.errors.palavraEsperanca && (
+            <span className="error">{formik.errors.palavraEsperanca}</span>
+          )}
+          <CurrentDataPreview
+            label={"mensagem atual:"}
+            value={apiData?.valor?.mensagem || ""}
+            loadingContent={loadingContent}
+            showCurrentData={showCurrentData}
+          />
+        </div>
+        <div className="form-group">
+          <label className="label-ide" htmlFor="textoBiblico">Texto Bíblico *</label>
+          <textarea
+            id="textoBiblico"
+            name="textoBiblico"
+            rows="4"
+            placeholder="Digite aqui o versículo ou texto bíblico"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.textoBiblico}
+          ></textarea>
+          {formik.touched.textoBiblico && formik.errors.textoBiblico && (
+            <span className="error">{formik.errors.textoBiblico}</span>
+          )}
+          <CurrentDataPreview
+            label={"versiculo atual:"}
+            value={apiData?.valor?.versiculo || ""}
+            loadingContent={loadingContent}
+            showCurrentData={showCurrentData}
+            isTextArea={true}
+          />
+        </div>
+        <div className="form-group">
+          <label className="label-ide" htmlFor="referencia">Referência Bíblica *</label>
+          <input
+            id="referencia"
+            name="referencia"
+            type="text"
+            placeholder='Ex: "João 3:16"'
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.referencia}
+          />
+          {formik.touched.referencia && formik.errors.referencia && (
+            <span className="error">{formik.errors.referencia}</span>
+          )}
+          <CurrentDataPreview
+            label={"referência atual:"}
+            value={apiData?.valor?.referencia || ""}
+            loadingContent={loadingContent}
+            showCurrentData={showCurrentData}
+          />
+        </div>
+        <div className="form-group">
+          <label className="label-ide" htmlFor="background">Imagem de Fundo *</label>
+          <select
+            id="background"
+            name="background"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.background}
+            className={
+              formik.touched.background && formik.errors.background
+                ? "input-error"
+                : ""
+            }
+          >
+            <option value="" disabled>
+              Selecione uma opção
             </option>
-          ))}
-        </select>
-        {formik.touched.background && formik.errors.background && (
-          <span className="error">{formik.errors.background}</span>
-        )}
-
-        <CurrentDataPreview
-          label={"imagem de fundo atual:"}
-          value={apiData?.valor?.background || ""}
-          loadingContent={loadingContent}
-          showCurrentData={showCurrentData}
-        />
-      </div>
-
-      <div className="form-group">
-        <label className="label-ide" htmlFor="louvor">Link do Louvor *</label>
-
-        <input
-          type="text"
-          placeholder="Link do louvor (do youtube) aqui"
-          id="louvor"
-          name="louvor"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.louvor}
-          className={
-            formik.touched.louvor && formik.errors.louvor ? "input-error" : ""
-          }
-        />
-        {formik.touched.louvor && formik.errors.louvor && (
-          <span className="error">{formik.errors.louvor}</span>
-        )}
-
-        <CurrentDataPreview
-          label={"link do louvor atual:"}
-          value={apiData?.valor?.link_louvor || ""}
-          loadingContent={loadingContent}
-          showCurrentData={showCurrentData}
-        />
-      </div>
-
-      <div className="form-group">
-        <label className="label-ide" htmlFor="tema">Tema de Cores *</label>
-
-        <select
-          id="tema"
-          name="tema"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.tema}
-          className={
-            formik.touched.tema && formik.errors.tema ? "input-error" : ""
-          }
-        >
-          <option value="" disabled>
-            Selecione um Tema
-          </option>
-          {temaOptions.map((option) => (
-            <option key={option.value} value={option.value}>
-              {option.label}
+            {backgroundOptions.map((option) => (
+              <option key={option.value} value={option.value}>
+                {option.label}
+              </option>
+            ))}
+          </select>
+          {formik.touched.background && formik.errors.background && (
+            <span className="error">{formik.errors.background}</span>
+          )}
+          <CurrentDataPreview
+            label={"imagem de fundo atual:"}
+            value={apiData?.valor?.background || ""}
+            loadingContent={loadingContent}
+            showCurrentData={showCurrentData}
+          />
+        </div>
+        <div className="form-group">
+          <label className="label-ide" htmlFor="louvor">Link do Louvor *</label>
+          <input
+            type="text"
+            placeholder="Link do louvor (do youtube) aqui"
+            id="louvor"
+            name="louvor"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.louvor}
+            className={
+              formik.touched.louvor && formik.errors.louvor ? "input-error" : ""
+            }
+          />
+          {formik.touched.louvor && formik.errors.louvor && (
+            <span className="error">{formik.errors.louvor}</span>
+          )}
+          <CurrentDataPreview
+            label={"link do louvor atual:"}
+            value={apiData?.valor?.link_louvor || ""}
+            loadingContent={loadingContent}
+            showCurrentData={showCurrentData}
+          />
+        </div>
+        <div className="form-group">
+          <label className="label-ide" htmlFor="tema">Tema de Cores *</label>
+          <select
+            id="tema"
+            name="tema"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.tema}
+            className={
+              formik.touched.tema && formik.errors.tema ? "input-error" : ""
+            }
+          >
+            <option value="" disabled>
+              Selecione um Tema
             </option>
-          ))}
-        </select>
-        {formik.touched.tema && formik.errors.tema && (
-          <span className="error">{formik.errors.tema}</span>
-        )}
-
-        <CurrentDataPreview
-          label={"tema atual:"}
-          value={apiData?.valor?.tema || ""}
-          loadingContent={loadingContent}
-          showCurrentData={showCurrentData}
-        />
+            {temaOptions.map((option) => (
+              <option key={option.value} value={option.value}>
+                {option.label}
+              </option>
+            ))}
+          </select>
+          {formik.touched.tema && formik.errors.tema && (
+            <span className="error">{formik.errors.tema}</span>
+          )}
+          <CurrentDataPreview
+            label={"tema atual:"}
+            value={apiData?.valor?.tema || ""}
+            loadingContent={loadingContent}
+            showCurrentData={showCurrentData}
+          />
+        </div>
+        <button type="submit" disabled={loadingUpdateContent}>
+          {loadingUpdateContent ? "Salvando..." : "Proclamar a Mensagem 📖"}
+        </button>
       </div>
-
-      <button type="submit" disabled={loadingUpdateContent}>
-        {loadingUpdateContent ? "Salvando..." : "Proclamar a Mensagem 📖"}
-      </button>
     </form>
   );
 };
